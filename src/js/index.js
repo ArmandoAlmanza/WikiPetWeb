0736624702573f07090023413795e4535f849d16
Armando Almanza <armando.s.almanza@gmail.com>
2022-04-23 17:05:04 -0500

    Add the tip content

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,24 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    console.log('DOM fully loaded and parsed');
+document.addEventListener("DOMContentLoaded", (event) => {
+    app();
 });
+
+const app = () => {
+    changeTip();
+};
+
+const changeTip = () => {
+    const tipP = document.getElementById("tip");
+
+    const tips = [
+        "Cudia a tu perro",
+        "Sacalo a pasear diario",
+        "Dale aguita caliente",
+        "aaaaaaaaaaaaaaaaa",
+        "Dale de comer a una hora adecuada",
+    ];
+
+    setInterval(
+        () => (tipP.innerHTML = tips[Math.floor(Math.random() * 4)]),
+        5000
+    );
+};
